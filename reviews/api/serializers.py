33817e86d7b793e7fb599529051c6e623b8bee74
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2024-12-03 01:58:44 +0100

    reviews app testing

--- reviews/api/serializers.py
+++ reviews/api/serializers.py
@@ -5,8 +5,7 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = '__all__'
-        read_only_fields = ['id', 'business_user', 'reviewer', 'created_at']
-
+        
     def partial_update(self, instance, validated_data):
         """
         Only allows rating and description for PATCH.
